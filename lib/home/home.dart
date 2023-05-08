56fa4ef7926a05c2bfa25550d6882a024210539f
Angel Mambel <angelmambel@MacBook-Pro-de-Angel.local>
2023-05-08 00:35:23 -0400

    agregando assets y diseniando hom

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -7,8 +7,52 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
+        const SliverAppBar(
+          title: Text('home'),
+        ),
         SliverToBoxAdapter(
-          child: Center(child: Text('home')),
+          child: Container(
+            height: 150,
+            decoration: BoxDecoration(
+              
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  child: Image.asset(
+                    'assets/img/NAVE1.png',
+                    height: 150,
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: Image.asset(
+                    'assets/img/NAVE2.png',
+                    height: 150,
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  left: 0,
+                  child: Center(
+                    child: Image.asset(
+                      'assets/img/ALIEN.png',
+                      height: 100,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SliverToBoxAdapter(
+          child: SafeArea(
+            child: Center(child: Text('home'))
+          ),
         )
       ],
     );
